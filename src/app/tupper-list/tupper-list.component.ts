113406ba2d9c270dab7ef835112305206178f582
JuanMolfese <juanjosemolfese@gmail.com>
2020-10-15 21:16:06 -0300

    Se agrega interface Products, y al objeto se le agregaron los tipos a cada atributo, tmb se agregaron directivas y pipes al template html, se crea en css contenido para las ofertas y finalmente un moke de productos para probar la funcionalidad

--- src/app/tupper-list/tupper-list.component.ts
+++ src/app/tupper-list/tupper-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from './product';
 
 @Component({
   selector: 'app-tupper-list',
@@ -7,13 +8,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TupperListComponent implements OnInit {
 
-  product ={
-      "name":"Practi Frezze",
-      "type":"freezer",
-      "price":249,
-      "stock":5,
-      "image":"assets/img/practi_freeze500.jpg"
-  }
+  products: Product [] = [
+    
+    { name:'Practi Frezze',
+      type:'freezer',
+      price: 289,
+      stock: 5,
+      image:'assets/img/practi_freeze500.jpg',
+      clearance: false,
+    },
+    { name:'Eco Twist',
+      type:'heladera',
+      price: 299,
+      stock: 3,
+      image:'assets/img/eco_twist500.png',
+      clearance: true,
+    },
+    { name:'Poeme',
+      type:'heladera',
+      price: 199,
+      stock: 0,
+      image:'assets/img/poeme.png',
+      clearance: false,
+    }
+  ];
 
   constructor() { }
 
