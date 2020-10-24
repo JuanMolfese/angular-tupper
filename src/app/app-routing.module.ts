3f3afa63f85c6aba7b45b43b3d99f9f023005849
JuanMolfese <juanjosemolfese@gmail.com>
2020-10-24 16:28:21 -0300

    se implemento el router y la pagina de contacto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TupperContactComponent } from "./tupper-contact/tupper-contact.component";
+import { TupperMainComponent } from './tupper-main/tupper-main.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: TupperMainComponent},
+  { path: 'main', component: TupperMainComponent},
+  { path: 'contact', component: TupperContactComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
