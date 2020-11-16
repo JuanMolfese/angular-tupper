c8acab38ad9c96434ced1c8d033df361702d1cc0
JuanMolfese <juanjosemolfese@gmail.com>
2020-11-16 11:07:06 -0300

    queda funcion al suma de productos en agregados al carrito, se da estilo a acho de columnas para evitar desplazamientos al agregar o quitar itmes del tupper list. Se detecta bug al tener items en carrito y pasar a contacto .. al volver el carrito obviamente esta cargado pero las var de la lista se setearon

--- src/app/tupper-shopping-cart.service.ts
+++ src/app/tupper-shopping-cart.service.ts
@@ -2,31 +2,30 @@ import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 import { Product } from './tupper-list/product';
 
-
-
 @Injectable({
   providedIn: 'root'
 })
 export class TupperShoppingCartService {
   
   constructor() { }
+  
   private _buyList : Product[] = [];
   buyList: BehaviorSubject <Product[]> = new BehaviorSubject([]);
+ 
   private _total: number = 0;
   total :BehaviorSubject<number> = new BehaviorSubject<number>(this._total);
 
   addToCart(product: Product) {
 
-    this._total += product.price*product.quantity;
-    
+    this._total += product.price*product.quantity;    
     let item : Product = this._buyList.find((elemento => elemento.name == product.name));
+
     //Si no esta ese producto lo agrega
     if(!item){
       this._buyList.push({ ... product});  
     }else{
       item.quantity += product.quantity;
-    }
-    
+    }    
     this.buyList.next(this._buyList); //Le dice al Behavior que notifique el nuevo valor a la variable privada (equivale al emmiter de eventos).
     this.total.next(this._total);//Le dice al Behavior que notifique el nuevo valor a la variable privada (equivale al emmiter de eventos).
   }
